4e1d1a955f564c22bafa1daa3c025574a02d6066
matt11matthew <matt11matthew@gmail.com>
2017-06-13 12:03:13 -0400

    Initial commit

---
+++ src/main/java/com/cryptickits/kitpvp/CrypticKitPvP.java
@@ -0,0 +1,17 @@
+package com.cryptickits.kitpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrypticKitPvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
